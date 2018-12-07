e83667c22fa7e58716676bddbd23e8a34045cdb7
yyIntsuki <intsuki.yz@gmail.com>
2018-12-07 12:25:25 +0100

    Added support for 3 pages.

--- controllers/home.js
+++ controllers/home.js
@@ -4,3 +4,11 @@ var path = require('path');
 exports.Index = function(req,res){
     res.sendFile(path.resolve('public/index.html'));
 };
+
+exports.Profile = function(req,res){
+    res.sendFile(path.resolve('public/profile.html'));
+};
+
+exports.About = function(req,res){
+    res.sendFile(path.resolve('public/about.html'));
+};
--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var homeController = require('../controllers/home');
 
-router.get('/', homeController.Index);
+router.get('/hem', homeController.Index);
+router.get('/profil', homeController.Profile);
+router.get('/info', homeController.About);
 
 module.exports = router;
