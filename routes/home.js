1ef59685bf5166b272faa1818c7ef71429b9dc1c
AvutE <tuva@etavo.com>
2018-12-14 14:27:04 +0100

    Need to change branch really quick

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var homeController = require('../controllers/home');
 
 router.get('/', homeController.Login);
-router.get('/Index', homeController.Index);
+router.get('/index', homeController.Index);
 router.get('/profil', homeController.Profile);
 router.get('/info', homeController.About);
 
