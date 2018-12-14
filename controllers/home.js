3dfdf94426cd0b100c75e176b2656b395df86bf2
AvutE <tuva@etavo.com>
2018-12-14 10:11:32 +0100

    start to login

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,10 @@
 // get upm package path from node_modules.
 var path = require('path');
 
+exports.Login = function(req, res){
+    res.sendFile(path.resolve('public/login.html'))
+}
+
 exports.Index = function(req,res){
     res.sendFile(path.resolve('public/index.html'));
 };
--- routes/home.js
+++ routes/home.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var homeController = require('../controllers/home');
 
-router.get('/', homeController.Index);
+router.get('/', homeController.Login);
+router.get('/Index', homeController.Index);
 router.get('/profil', homeController.Profile);
 router.get('/info', homeController.About);
 
