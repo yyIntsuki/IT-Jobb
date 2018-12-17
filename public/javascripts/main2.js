3dd555b57f7542db3773bd810560f08afb877d16
IsakHuddinge <isak.nywall@edu.huddinge.se>
2018-12-17 11:17:12 +0100

    get user location

--- public/javascripts/main2.js
+++ public/javascripts/main2.js
@@ -1,6 +1,30 @@
+$( function(){
+    getUserLocation(function(userLocation){
+        
+    });
+});
 
+// Ask's user for their current location
+function getUserLocation(callback){
+    var allowsNavigator,
+        coordinates;
 
+    if(navigator.geolocation){
+        allowsNavigator = true;
 
-$(async function(){
+        navigator.geolocation.getCurrentPosition(function(position){
+            coordinates = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            callback({
+                allowsNavigator: allowsNavigator,
+                coordinates: coordinates
+            });
+        });
+    }else{
+
+    }
     
-})
+};
