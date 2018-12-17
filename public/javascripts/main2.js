e807e1f0c10505fe0e11fd9db686bc926db115a6
IsakHuddinge <isak.nywall@edu.huddinge.se>
2018-12-17 11:48:22 +0100

    show map on page load.

--- public/javascripts/main2.js
+++ public/javascripts/main2.js
@@ -1,18 +1,18 @@
-// Global variables
+// Global variables.
 var map,
     infowindow;
 
+// Main Function, Runs on page load.
 $(function(){
+    initMap();
     getUserLocation(function(userLocation){
         if(userLocation.allowsNavigator){
-            $('#searchForm').hide();
-            $('#map').show();
-            initMap(userLocation.coordinates);
+            centerMap(userLocation.coordinates);
         };
     });
 });
 
-// Ask's user for their current location
+// Ask's user for their current location.
 function getUserLocation(callback){
     var allowsNavigator,
         coordinates;
@@ -36,11 +36,26 @@ function getUserLocation(callback){
     };
 };
 
-// Generate a google map
-function initMap(coordinates){
+// Generate a google map.
+function initMap(){
+    var stockholm = {
+        lat: 59.3325800,
+        lng: 18.0649000
+    }
     // jquery selector returns a collection, we select the first element in it.
     map = new google.maps.Map($('#map')[0], {
-        center: coordinates,
-        zoom: 15
+        center: stockholm,
+        zoom: 13
     });
+    infoWindow = new google.maps.InfoWindow();
 }
+
+// Centers map at user location and opens a infowindow.
+function centerMap(coordinates){
+    map.setCenter(coordinates)
+
+    infoWindow.setPosition(coordinates);
+    infoWindow.setContent("Du är här!");
+    infoWindow.open(map);
+}
+
